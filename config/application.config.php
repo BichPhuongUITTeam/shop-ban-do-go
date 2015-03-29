e554efeaf6575026b7a643b2774e787f9c919eea
Le Minh Tri <ansidev@gmail.com>
2015-03-29 12:40:25 +0700

    Update Module config

--- config/application.config.php
+++ config/application.config.php
@@ -2,11 +2,11 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'Application',
-        // 'Album',
         'User',
+        'Application',
+        'Album',
         'ZendDeveloperTools',
-        // 'BjyProfiler',
+        'BjyProfiler',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
