db41d35439818d23b2788d1c6ab23ef00fbc3de4
Le Minh Tri <ansidev@gmail.com>
2015-03-30 20:24:36 +0700

    Set default layout.

--- module/User/config/module.config.php
+++ module/User/config/module.config.php
@@ -38,7 +38,8 @@ return array(
 
     'view_manager' => array(
         'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/custom_layout.phtml',
+            // 'layout/layout'           => __DIR__ . '/../view/layout/custom_layout.phtml',
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
