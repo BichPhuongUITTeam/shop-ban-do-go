13e3d06bda985b218d3eec516f4f5450d0753130
ansidev <ansidev@gmail.com>
2015-04-03 21:56:10 +0700

    Enable San Session Toolbar

--- config/application.config.php
+++ config/application.config.php
@@ -7,6 +7,7 @@ return array(
         'User',
         'ZendDeveloperTools',
         'BjyProfiler',
+        'SanSessionToolbar',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
