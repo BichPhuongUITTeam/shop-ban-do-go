e838f08adee080b7c12c699736fd9f14e7af634f
ansidev <ansidev@gmail.com>
2015-04-04 03:39:20 +0700

    Update `loginAction`, `logoutAction`. Now you can login and logout.

--- module/User/src/User/Controller/UserController.php
+++ module/User/src/User/Controller/UserController.php
@@ -2,13 +2,36 @@
 namespace User\Controller;
 
 use User\Form\UserForm;
+use User\Form\LoginForm;
 use User\Model\User;
+
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+
+//use Zend\Authentication\AuthenticationService;
+//use Zend\Authentication\Adapter\DbTable as DbTableAuthAdapter;
 
 class UserController extends AbstractActionController
 {
     protected $userTable;
+    protected $storage;
+    protected $authService;
+
+    public function getAuthService()
+    {
+        if (!$this->authService) {
+            $this->authService = $this->getServiceLocator()->get('AuthService');
+        }
+        return $this->authService;
+    }
+
+    public function getSessionStorage()
+    {
+        if (!$this->storage) {
+            $this->storage = $this->getServiceLocator()->get('User\Model\AppAuthStorage');
+        }
+        return $this->storage;
+    }
 
     public function getUserTable()
     {
@@ -22,16 +45,54 @@ class UserController extends AbstractActionController
 
     public function indexAction()
     {
+        if (!$this->getAuthService()->hasIdentity()) {
+            return $this->redirect()->toRoute('user', array('action' => 'login'));
+        }
         $view = new ViewModel(array(
             'users' => $this->getUserTable()->fetchAll(),
         ));
-        // $view->setTemplate('user/user/default.phtml');
-        // $this->layout()->setTemplate('layout/layout.phtml');
+//         $view->setTemplate('user/user/default.phtml');
+//         $this->layout()->setTemplate('layout/custom_layout.phtml');
         // $view->setTerminal(true);
         return $view;
     }
 
-    public function registerAction() {
+    public function loginAction()
+    {
+        if ($this->getAuthService()->hasIdentity()) {
+            return $this->redirect()->toRoute('user', array('action' => 'index'));
+        }
+        $view = new ViewModel();
+        $form = new LoginForm();
+        $form->get('submit')->setValue('Login');
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $user = new User();
+            $form->setInputFilter($user->getLoginInputFilter());
+            $form->setData($request->getPost());
+            if ($form->isValid()) {
+                $user->exchangeArray($form->getData());
+                $login = $this->getUserTable()->loginUser($user);
+                if ($login) {
+                    // Check if user select Remember me
+                    if ($request->getPost('remember_me') == 1) {
+                        // Set storage again
+                        $this->getSessionStorage()->setRememberMe(1);
+                    }
+                    $this->getAuthService()->getStorage()->write($request->getPost('username'));
+                    return $this->redirect()->toRoute('user', array('action' => 'index'));
+                }
+            }
+        }
+        // $view->setTemplate('users/index/login');
+        return array(
+            'form' => $form,
+            'view' => $view,
+        );
+    }
+
+    public function registerAction()
+    {
         $view = new ViewModel();
 //        $view->setTemplate('user/user/add.phtml')
 //        $view->setTerminal(true);
@@ -47,28 +108,27 @@ class UserController extends AbstractActionController
             if ($form->isValid()) {
                 $user->exchangeArray($form->getData());
                 $this->getUserTable()->saveUser($user);
-
-                return $this->redirect()->toRoute('user');
+                return $this->redirect()->toRoute('user', array('action' => 'index'));
             }
         }
-
         return array(
             'form' => $form,
-            'view' => $view
+            'view' => $view,
         );
     }
 
-    public function loginAction() {
-        // echo 'Đây là trang đăng nhập';
-        $view = new ViewModel();
-        // $view->setTemplate('users/index/login');
-        return $view;
+    public function logoutAction()
+    {
+        $this->getSessionStorage()->forgetMe();
+        $this->getAuthService()->clearIdentity();
+        $this->flashMessenger()->addMessage("You 've been logged out!");
+        return $this->redirect()->toRoute('user', array('action' => 'login'));
     }
 
     public function addAction()
     {
         $view = new ViewModel();
-        $view->setTemplate('user/user/add.phtml');        
+        $view->setTemplate('user/user/add.phtml');
         $form = new UserForm();
         $form->get('submit')->setValue('Add');
 
@@ -89,7 +149,7 @@ class UserController extends AbstractActionController
         return array(
             'form' => $form,
             'view' => $view
-            );
+        );
     }
 
     public function editAction()
