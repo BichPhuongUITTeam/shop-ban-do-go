c493f184dcf44fc5bfb7f2acb6018bed12ac5d30
ansidev <ansidev@gmail.com>
2015-04-05 01:17:01 +0700

    Update controller to fix authentication feature.

--- module/Users/src/Users/Controller/UsersController.php
+++ module/Users/src/Users/Controller/UsersController.php
@@ -16,17 +16,12 @@ class UsersController extends AbstractActionController
     protected $storage;
     protected $authService;
 
-    public function onDispatch(\Zend\Mvc\MvcEvent $e)
+    public function isLoggedIn()
     {
-//        $this->checkAuthentication();
-        return parent::onDispatch($e);
-    }
-
-    public function checkAuthentication()
-    {
-        if (!$this->getAuthService()->hasIdentity()) {
-            return $this->logoutAction();
+        if ($this->getAuthService()->hasIdentity()) {
+            return TRUE;
         }
+        return FALSE;
     }
 
     public function getAuthService()
@@ -57,31 +52,39 @@ class UsersController extends AbstractActionController
 
     public function indexAction()
     {
-        if (!$this->getAuthService()->hasIdentity()) {
-            return $this->logoutAction();
-        }
-        $view = new ViewModel(array(
-            'users' => $this->getUsersTable()->fetchAll(),
-        ));
+        $loggedIn = $this->isLoggedIn();
+        if ($loggedIn) {
+
+            $view = new ViewModel();
+            $users = $this->getUsersTable()->fetchAll();
 //         $view->setTemplate('users/users/default.phtml');
 //         $this->layout()->setTemplate('layout/custom_layout.phtml');
-        // $view->setTerminal(true);
-        return $view;
+            // $view->setTerminal(true);
+            return array(
+                'view' => $view,
+                'users' => $users,
+                'messages' => $this->flashMessenger()->getMessages(),
+            );
+        }
+        return $this->logoutAction();
     }
 
     public function loginAction()
     {
-        if ($this->getAuthService()->hasIdentity()) {
-            return $this->redirect()->toRoute('users', array('action' => 'index'));
+        $loggedIn = $this->isLoggedIn();
+        if ($loggedIn) {
+            return $this->indexAction();
+        } else {
+            $view = new ViewModel();
+            $form = new LoginForm();
+            $form->get('submit')->setValue('Login');
+            // $view->setTemplate('users/index/login');
+            return array(
+                'form' => $form,
+                'view' => $view,
+                'messages' => $this->flashMessenger()->getMessages(),
+            );
         }
-        $view = new ViewModel();
-        $form = new LoginForm();
-        $form->get('submit')->setValue('Login');
-        // $view->setTemplate('users/index/login');
-        return array(
-            'form' => $form,
-            'view' => $view,
-        );
     }
 
     public function authenticateAction()
@@ -90,35 +93,32 @@ class UsersController extends AbstractActionController
         $form = new LoginForm();
         $form->get('submit')->setValue('Login');
         $redirect = 'login';
+        $session = array(
+            'username' => NULL,
+            'logged_in' => FALSE,
+        );
         $request = $this->getRequest();
+//        var_dump($request->isPost()); die;
         if ($request->isPost()) {
             $user = new Users();
             $form->setInputFilter($user->getLoginInputFilter());
             $form->setData($request->getPost());
             if ($form->isValid()) {
-                $auth = $this->getAuthService();
-                $password_hash = $this->getUsersTable()->getUserPasswordHash($request->getPost('username'));
-                $auth->getAdapter()
-                    ->setIdentity($request->getPost('username'))
-                    ->setCredential($request->getPost('password'))
-                    ->setCredentialTreatment(password_verify($request->getPost('password'), $password_hash));
-                $result = $auth->authenticate();
-                foreach ($result->getMessages() as $message) {
-                    $this->flashMessenger()->addMessage($message);
-                }
-
-                if ($result->isValid()) {
+                $user->exchangeArray($form->getData());
+                $result = $this->getUsersTable()->loginUser($user);
+                if ($result) {
                     $redirect = 'index';
                     // Check if user select Remember me
                     if ($request->getPost('remember_me') == 1) {
                         // Set storage again
                         $this->getSessionStorage()->setRememberMe(1);
                     }
-                    $this->getAuthService()->getStorage()->write(array(
-                        'username' => $request->getPost('username'),
-                        'logged_in' => TRUE,
-                    ));
-
+                    $this->getSessionStorage()->setRememberMe();
+                    $session['username'] = $request->getPost('username');
+                    $session['logged_in'] = TRUE;
+                    $fullname = $this->getUsersTable()->getUserFullName($request->getPost('username'));
+                    $message = "Welcome, <strong>" . $fullname . "</strong>";
+                    $this->flashMessenger()->addMessage($message);
                 }
 //                $user->exchangeArray($form->getData());
 //                $login = $this->getUsersTable()->loginUser($user);
@@ -126,35 +126,40 @@ class UsersController extends AbstractActionController
 //                }
             }
         }
-        $this->getAuthService()->getStorage()->write($redirect);
+        $this->getAuthService()->getStorage()->write($session);
         return $this->redirect()->toRoute('users', array('action' => $redirect));
     }
 
     public
     function registerAction()
     {
-        $view = new ViewModel();
+        $loggedIn = $this->isLoggedIn();
+        if ($loggedIn) {
+            return $this->redirect()->toRoute('home');
+        } else {
+            $view = new ViewModel();
 //        $view->setTemplate('users/users/index.phtml');
 //        $view->setTerminal(true);
-        $form = new UserForm();
-        $form->get('submit')->setValue('Register');
+            $form = new UserForm();
+            $form->get('submit')->setValue('Register');
 
-        $request = $this->getRequest();
-        if ($request->isPost()) {
-            $user = new Users();
-            $form->setInputFilter($user->getInputFilter());
-            $form->setData($request->getPost());
+            $request = $this->getRequest();
+            if ($request->isPost()) {
+                $user = new Users();
+                $form->setInputFilter($user->getInputFilter());
+                $form->setData($request->getPost());
 
-            if ($form->isValid()) {
-                $user->exchangeArray($form->getData());
-                $this->getUsersTable()->saveUser($user);
-                return $this->redirect()->toRoute('users', array('action' => 'index'));
+                if ($form->isValid()) {
+                    $user->exchangeArray($form->getData());
+                    $this->getUsersTable()->saveUser($user);
+                    return $this->redirect()->toRoute('users', array('action' => 'index'));
+                }
             }
+            return array(
+                'form' => $form,
+                'view' => $view,
+            );
         }
-        return array(
-            'form' => $form,
-            'view' => $view,
-        );
     }
 
     public
@@ -169,100 +174,113 @@ class UsersController extends AbstractActionController
     public
     function addAction()
     {
-        $view = new ViewModel();
-        $view->setTemplate('users/users/add.phtml');
-        $form = new UserForm();
-        $form->get('submit')->setValue('Add');
+        $loggedIn = $this->isLoggedIn();
+        if (!$loggedIn) {
+            return $this->logoutAction();
+        } else {
+            $view = new ViewModel();
+            $view->setTemplate('users/users/add.phtml');
+            $form = new UserForm();
+            $form->get('submit')->setValue('Add');
 
-        $request = $this->getRequest();
-        if ($request->isPost()) {
-            $user = new Users();
-            $form->setInputFilter($user->getInputFilter());
-            $form->setData($request->getPost());
+            $request = $this->getRequest();
+            if ($request->isPost()) {
+                $user = new Users();
+                $form->setInputFilter($user->getInputFilter());
+                $form->setData($request->getPost());
 
-            if ($form->isValid()) {
-                $user->exchangeArray($form->getData());
-                $this->getUsersTable()->saveUser($user);
+                if ($form->isValid()) {
+                    $user->exchangeArray($form->getData());
+                    $this->getUsersTable()->saveUser($user);
 
-                return $this->redirect()->toRoute('users', array('action' => 'index'));
+                    return $this->redirect()->toRoute('users', array('action' => 'index'));
+                }
             }
-        }
 
-        return array(
-            'form' => $form,
-            'view' => $view
-        );
+            return array(
+                'form' => $form,
+                'view' => $view
+            );
+        }
     }
 
     public
     function editAction()
     {
-        if (!$this->getAuthService()->hasIdentity()) {
-            return $this->redirect()->toRoute('users', array('action' => 'login'));
-        }
-        $id = (int)$this->params()->fromRoute('id', 0);
+        $loggedIn = $this->isLoggedIn();
+        if (!$loggedIn) {
+            return $this->logoutAction();
+        } else {
+            if (!$this->getAuthService()->hasIdentity()) {
+                return $this->redirect()->toRoute('users', array('action' => 'login'));
+            }
+            $id = (int)$this->params()->fromRoute('id', 0);
 
-        if (!$id) {
-            return $this->redirect()->toRoute('users', array('action' => 'add'));
-        }
+            if (!$id) {
+                return $this->redirect()->toRoute('users', array('action' => 'add'));
+            }
 
 //        Get the user with specification id.
 //        An exception is thrown if it cannot be found, in which case go to index page.
-        try {
-            $user = $this->getUsersTable()->getUserById($id);
-        } catch (\Exception $ex) {
-            return $this->redirect()->toRoute('users', array('action' => 'index'));
+            try {
+                $user = $this->getUsersTable()->getUserById($id);
+            } catch (\Exception $ex) {
+                return $this->redirect()->toRoute('users', array('action' => 'index'));
+            }
+
+            $form = new UserForm();
+            $form->bind($user);
+            $form->get('submit')->setAttribute('value', 'Edit');
+
+            $request = $this->getRequest();
+            if ($request->isPost()) {
+                $form->setInputFilter($user->getInputFilter());
+                $form->setData($request->getPost());
+
+                if ($form->isValid()) {
+                    $this->getUsersTable()->saveUser($user);
+
+                    // Redirect to list of user
+                    return $this->redirect()->toRoute('users');
+                }
+            }
+
+            return array(
+                'id' => $id,
+                'form' => $form,
+            );
         }
+    }
 
-        $form = new UserForm();
-        $form->bind($user);
-        $form->get('submit')->setAttribute('value', 'Edit');
+    public function deleteAction()
+    {
+        $loggedIn = $this->isLoggedIn();
+        if (!$loggedIn) {
+            return $this->logoutAction();
+        } else {
+            $id = (int)$this->params()->fromRoute('id', 0);
 
-        $request = $this->getRequest();
-        if ($request->isPost()) {
-            $form->setInputFilter($user->getInputFilter());
-            $form->setData($request->getPost());
+            if (!$id) {
+                return $this->redirect()->toRoute('users', array('action' => 'index'));
+            }
 
-            if ($form->isValid()) {
-                $this->getUsersTable()->saveUser($user);
+            $request = $this->getRequest();
+            if ($request->isPost()) {
+                $del = $request->getPost('del', 'No');
+
+                if ($del == 'Yes') {
+                    $id = (int)$request->getPost('id');
+                    $this->getUsersTable()->deleteUser($id);
+                }
 
                 // Redirect to list of user
                 return $this->redirect()->toRoute('users');
             }
+
+            return array(
+                'id' => $id,
+                'user' => $this->getUsersTable()->getUserById($id),
+            );
         }
-
-        return array(
-            'id' => $id,
-            'form' => $form,
-        );
-
-    }
-
-    public
-    function deleteAction()
-    {
-        $id = (int)$this->params()->fromRoute('id', 0);
-
-        if (!$id) {
-            return $this->redirect()->toRoute('users', array('action' => 'index'));
-        }
-
-        $request = $this->getRequest();
-        if ($request->isPost()) {
-            $del = $request->getPost('del', 'No');
-
-            if ($del == 'Yes') {
-                $id = (int)$request->getPost('id');
-                $this->getUsersTable()->deleteUser($id);
-            }
-
-            // Redirect to list of user
-            return $this->redirect()->toRoute('users');
-        }
-
-        return array(
-            'id' => $id,
-            'user' => $this->getUsersTable()->getUserById($id),
-        );
     }
 }
